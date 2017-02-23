44dfa874e9e05c15670b627c6473c83c35d88325
margaretmcrawf <margaretmcrawf@gmail.com>
2017-02-22 23:42:30 -0500

    can't figure out tee

---
+++ exercises/ex03/tee.c
@@ -0,0 +1,13 @@
+""" My version of the tee function
+	inputs: :/
+	I don't think I understand the tee command well enough to duplicate it.
+"""
+
+int main(int argc, char *argv[]) {
+
+	int counter = 0;
+	while (counter < argc) {
+
+	}
+
+}
